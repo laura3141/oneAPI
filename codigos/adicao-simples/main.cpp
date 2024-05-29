c4b87078acc9681a3c4fb351ca677c2183e3f5d9
Laura <laura.costa3141@gmail.com>
2024-05-29 18:39:49 -0300

    update

---
+++ codigos/adicao-simples/main.cpp
@@ -0,0 +1,36 @@
+#include <CL/sycl.hpp>
+#include <iostream>
+
+using namespace cl::sycl;
+using namespace std;
+int main(){
+
+    // tamanho do espaço de iteraçao
+    int N=10000000;
+    //range de 1 dimensao e 100 iterações
+    auto r= range<1>(N);
+    //criaçao do buffer passando o range criado 
+    buffer<int>a {r},b{r},c{r};
+    gpu_selector seletor;
+    queue q(seletor);
+    q.submit([&](handler & h ){
+        //acessadores para os buffers
+        accessor A(a,h,read_write);
+        accessor B(b,h,read_write);
+        accessor C(c,h,write_only);
+        
+        //kernel que sera executado
+        h.parallel_for(range<1>(N), [=](auto i){
+            A[i]=i;
+            B[i]=i;
+            C[i] = A[i]  B[i];
+        });
+    });
+    host_accessor aux(c,read_only);
+    for(int i=0;i<N;i++){
+        cout<<aux[i]<<std::endl;
+    }
+}
+    
+
+
--- codigos/calculadora/main.cpp
+++ codigos/calculadora/main.cpp
@@ -12,7 +12,8 @@ int main(){
     float result = 0;
 
     //default_selector device_selector;
-    cpu_selector selector;
+    //cpu_selector selector;
+    gpu_selector selector;
     queue q(selector);
 
     cout << "Running on " << q.get_device().get_info<info::device::name>() << "\n";
@@ -20,15 +21,27 @@ int main(){
     cin >> num1;
     cout << "Digite o num2: ";
     cin >> num2;
-    cout << "Digite a operação (+, -, *, /): ";
+    cout << "Digite a operação (+, -, *, /, >): ";
     cin >> op;
     
     buffer<float, 1> num1_buff(&num1, range<1>(1));
     buffer<float, 1> num2_buff(&num2, range<1>(1));
     buffer<char, 1> op_buff(&op, range<1>(1));
     buffer<float, 1> res_buff(&result, range<1>(1));
+    /*
+    Explicando range<>()
+    1.
+    buffer<float, 1> num1_buff(&num1, range<1>(1));
+    Uma unica dimesao e um vetor de apenas um elemento
+    2.
+    buffer<float, 1> num1_buff(&num1, range<1>(2));
+    Uma unica dimesao e um vetor dois elementos
+    3.
+    buffer<float, 2> num1_buff(&num1, range<2>(2,2));
+    Duas dimesoes ou seja uma matriz 2 por 2
+    */
 
-    queue{}.submit([&](handler& calc) {
+    q.submit([&](handler& calc) {
         accessor o{op_buff, calc}; //auto o = op_buff.get_access<access::mode::read>(calc);
         accessor n1{num1_buff, calc}; //auto n1 = num1_buff.get_access<access::mode::read>(calc);
         accessor n2{num2_buff, calc}; //auto n2 = num2_buff.get_access<access::mode::read>(calc);
@@ -37,7 +50,7 @@ int main(){
 
         
         
-        calc.single_task<class vector_addition>([=]() {
+        calc.single_task([=]() {
 
             if (op == '+') {
                 r[0] = n1[0] + n2[0];
@@ -47,16 +60,28 @@ int main(){
                 r[0] = n1[0] * n2[0];
             }else if(op == '/'){
                 r[0] = n1[0] / n2[0];
+            }else if(op == '>'){
+                int aux = 1;
+                while (-2 < aux)
+                {
+                    aux = aux * n1[0];
+                }
+                
+                /*for (int i = 0; i < n2[0]; i++)
+                {
+                    aux = aux * n1[0];
+                }*/
+                r[0] = aux;
+            }else{
+                r[0] = 343;
             }
         });
 
     });
-    
 
     //('basic_ostream<char, char_traits<char>>' and 'host_accessor<float, 1, access::mode::read_write>')
 
     host_accessor re{res_buff};
-    //cout << "blabla-bla\n";
     cout << "Res: " << result << "\n";
     q.wait();
 
